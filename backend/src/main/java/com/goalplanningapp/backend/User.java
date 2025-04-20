9789f0a5a8e67c016e6638bcec06ec9ec5bbf5f4
davidxosei <davidxosei@gmail.com>
2025-04-20 10:03:25 -0400

    added backend dependencies

---
+++ backend/src/main/java/com/goalplanningapp/backend/User.java
@@ -0,0 +1,29 @@
+// package com.goalplanningapp.backend;
+
+// import jakarta.persistence.Column;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.Id;
+
+// @Entity
+// public class User {
+
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private int id;
+    
+//     @Column(nullable = false)
+//     private String username;
+    
+//     @Column(nullable = false)
+//     private String password;
+
+//     public User(String username, String password) {
+//         this.username = username;
+//         this.password = password;
+//     }
+
+    
+
+// }
