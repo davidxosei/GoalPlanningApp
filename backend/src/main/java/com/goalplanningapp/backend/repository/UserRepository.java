082ed1d5ece75912a6550d1b7f1968148e2015a9
davidxosei <davidxosei@gmail.com>
2025-04-22 08:42:57 -0400

    created model, dtos, and user repository
    I plan to implement security features such as session authentication later but am allowing all endpoints to be publicly accessible for now.

---
+++ backend/src/main/java/com/goalplanningapp/backend/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.goalplanningapp.backend.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.goalplanningapp.backend.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+    Optional<User> findById(Long id);
+    Optional<User> findByUsername(String username);
+}
