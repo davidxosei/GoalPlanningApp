db827d35b5927507c5ad46181139d1a32d70e528
davidxosei <davidxosei@gmail.com>
2025-05-03 17:14:41 -0400

    Added environment variables for deployment

--- backend/src/main/java/com/goalplanningapp/backend/BackendApplication.java
+++ backend/src/main/java/com/goalplanningapp/backend/BackendApplication.java
@@ -9,9 +9,9 @@ import io.github.cdimascio.dotenv.Dotenv;
 public class BackendApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-		System.setProperty("JWT_SECRET_KEY",  dotenv.get("JWT_SECRET_KEY"));
+		// Dotenv dotenv = Dotenv.load();
+		// System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		// System.setProperty("JWT_SECRET_KEY",  dotenv.get("JWT_SECRET_KEY"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
